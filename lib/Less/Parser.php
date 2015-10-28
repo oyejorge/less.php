28855023bcaddb83c0801cca18fa913961ed87dd
Adrian Badarau <adi.badarau@yahoo.com>
2015-10-28 13:07:26 +0200

    Update Parser.php
    added a rgb2html function as the you passed hex values to the formatter and got rgb values

--- lib/Less/Parser.php
+++ lib/Less/Parser.php
@@ -277,10 +277,28 @@ class Less_Parser{
 			}
 		}
 		if(get_class($var->value->value[0]->value[0]) == "Less_Tree_Color"){
-			return implode(",",$var->value->value[0]->value[0]->rgb);
+			return $this->rgb2html($var->value->value[0]->value[0]->rgb);
 		}
 		return false;
 	}
+	
+	private function rgb2html($r, $g=-1, $b=-1)
+    	{
+	        if (is_array($r) && sizeof($r) == 3)
+	            list($r, $g, $b) = $r;
+	
+	        $r = intval($r); $g = intval($g);
+	        $b = intval($b);
+	
+	        $r = dechex($r<0?0:($r>255?255:$r));
+	        $g = dechex($g<0?0:($g>255?255:$g));
+	        $b = dechex($b<0?0:($b>255?255:$b));
+	
+	        $color = (strlen($r) < 2?'0':'').$r;
+	        $color .= (strlen($g) < 2?'0':'').$g;
+	        $color .= (strlen($b) < 2?'0':'').$b;
+	        return '#'.$color;
+    	}
 
 	/**
 	 * Run pre-compile visitors
