cfba744f708307d09bbe9462002c156549f60c42
Cerdic <cedric@yterium.com>
2018-10-16 11:03:29 +0200

    Less_Tree_Call: no local reference to currentFileInfo, using static Less_Environment::$currentFileInfo

--- lib/Less/Parser.php
+++ lib/Less/Parser.php
@@ -1213,7 +1213,7 @@ class Less_Parser{
 		}
 
 		if ($name) {
-			return $this->NewObj4('Less_Tree_Call',array($name, $args, $index, Less_Environment::$currentFileInfo) );
+			return $this->NewObj3('Less_Tree_Call',array($name, $args, $index) );
 		}
 	}
 
